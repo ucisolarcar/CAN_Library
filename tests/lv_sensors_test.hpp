59123383274034fa7496344cfd2c1a062f3b9908
bryanmelendez <bryanmelendez2323@gmail.com>
2025-04-14 12:30:17 -0700

    test file

---
+++ tests/lv_sensors_test.hpp
@@ -0,0 +1,18 @@
+#ifndef LV_SENSORS
+#define LV_SENSORS
+
+#include <iostream>
+
+class LV_TEST {
+private:
+
+public:
+    void currentTest();
+
+};
+
+void LV_TEST::currentTest() {
+    std::cout << "Test\n";
+}
+
+#endif
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -3,8 +3,14 @@
 // include library
 #include "sensorFunctions.h"
 
+// include tests
+#include "lv_sensors_test.hpp"
+
 int main() {
     std::cout << "This is the test file" << std::endl;
+
+    LV_TEST lv_test;
+    lv_test.currentTest();
 
     return 0;
 }
