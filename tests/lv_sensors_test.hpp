c9bb79855db9c9905bd85f977b394fae43a8be05
juliusya45 <derpyjul45@gmail.com>
2025-04-17 16:28:38 -0700

    put parseCurrent in sensorFunctions and changed the parameter for those functions

--- tests/lv_sensors_test.hpp
+++ tests/lv_sensors_test.hpp
@@ -5,6 +5,7 @@
 #include <iomanip>      // For hex printouts
 #include <iterator>     // To copy over can_frame to CAN
 #include "canFloat.h"
+#include"sensorFunctions.h"
 
 using namespace std;
 
@@ -13,17 +14,7 @@ private:
 
 public:
     void currentTest();
-    float parseCurrent(CAN currMessage);
 };
-
-/* Process would be get can_frame from the CANBUS network
-*  and then get the CAN struct (just the 8 bytes) and pass that into this library
-*  
-*/
-
-/*  Alternatively just pass the can_frame in
-*  
-*/
 
 
 void LV_TEST::currentTest() {
@@ -35,7 +26,7 @@ void LV_TEST::currentTest() {
     can_frame currCanFrame;
     currCanFrame.can_id = 0x702;    // CAN ID of 12V DC/DC line Current Sensor
     currCanFrame.can_dlc = 4;       // We should only send 4 bytes of data
-    currCanFrame.data[0] = 0x3e;    // Setting the data frame to have the float value of 0.125A
+    currCanFrame.data[0] = 0xbe;    // Setting the data frame to have the float value of 0.125A
     for (int i = 1; i <= 7; i++)
         currCanFrame.data[i] = 0;
 
@@ -51,28 +42,8 @@ void LV_TEST::currentTest() {
     // copy the array of bytes from currCanFrame to the array of bytes in canData
     copy(begin(currCanFrame.data), end(currCanFrame.data), begin(canData.canBytes));
     // Finally parse the current
-    parseCurrent(canData);
-}
-
-float LV_TEST::parseCurrent(CAN currMessage) {
-    cout << "Parsing the Current based off of CAN struct...\n";
-    cout << "----------------------------------------------\n";
-    // Creating the CanFloats object for conversion
-    CanFloats converter(currMessage);
-
-    cout << "CAN: \n";
-    for (int i = 0; i <= 7; i++)
-        cout << "Byte [" << i << "]: " << hex << (int) currMessage.canBytes[i] << "\n";
-
-
-    // get the floats back from the 
-    floatPair floats = converter.getFloats();
-    cout << "Float 1: " << floats.num1 << "\n";
-    cout << "Float 2: " << floats.num2 << "\n";
-
-    float current = floats.num1;
-    cout << "Current After Conversion (value that would be returned): " << current << "\n";
-    return current;
+    SensorFunctions parser;
+    parser.parseCurrent(canData, true);
 }
 
 #endif
