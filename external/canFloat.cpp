5603a3ffc3a21f0785d2a3e723b90d1957d4ecd4
juliusya45 <derpyjul45@gmail.com>
2025-04-16 23:58:45 -0700

    init formalTest. Changed CAN struct and canFloat class. Created a structure for formal testing

--- external/canFloat.cpp
+++ external/canFloat.cpp
@@ -13,6 +13,13 @@ CanFloats::CanFloats()
   this->myNum2.myFloat = 0;
 }
 
+CanFloats::CanFloats(CAN canMsg)
+{
+  floatPair pair = toFloatPair(canMsg);
+  this->myNum1.myFloat = pair.num1;
+  this->myNum2.myFloat = pair.num2;
+}
+
 floatPair CanFloats::getFloats()
 {
   floatPair pair = toFloatPair(getCAN());
@@ -38,16 +45,16 @@ floatPair CanFloats::toFloatPair(CAN canInput)
   floatUnion num1;
   floatUnion num2;
   // Recreate number 1
-  num1.myBytes[3] = canInput.byte1;
-  num1.myBytes[2] = canInput.byte2;
-  num1.myBytes[1] = canInput.byte3;
-  num1.myBytes[0] = canInput.byte4;
+  num1.myBytes[3] = canInput.canBytes[0];
+  num1.myBytes[2] = canInput.canBytes[1];
+  num1.myBytes[1] = canInput.canBytes[2];
+  num1.myBytes[0] = canInput.canBytes[3];
 
   // Recreate number 2:
-  num2.myBytes[3] = canInput.byte5;
-  num2.myBytes[2] = canInput.byte6;
-  num2.myBytes[1] = canInput.byte7;
-  num2.myBytes[0] = canInput.byte8;
+  num2.myBytes[3] = canInput.canBytes[4];
+  num2.myBytes[2] = canInput.canBytes[5];
+  num2.myBytes[1] = canInput.canBytes[6];
+  num2.myBytes[0] = canInput.canBytes[7];
 
   output.num1 = num1.myFloat;
   output.num2 = num2.myFloat;
@@ -59,14 +66,16 @@ CAN CanFloats::toCan(floatUnion num1, floatUnion num2)
 {
   //initialize the struct for the CAN, and copy the bits over into an array
   CAN canOut;
-  canOut.byte1 = num1.myBytes[3];
-  canOut.byte2 = num1.myBytes[2];
-  canOut.byte3 = num1.myBytes[1];
-  canOut.byte4 = num1.myBytes[0];
-  canOut.byte5 = num2.myBytes[3];
-  canOut.byte6 = num2.myBytes[2];
-  canOut.byte7 = num2.myBytes[1];
-  canOut.byte8 = num2.myBytes[0];
+
+  canOut.canBytes[0] = num1.myBytes[3];
+  canOut.canBytes[1] = num1.myBytes[2];
+  canOut.canBytes[2] = num1.myBytes[1];
+  canOut.canBytes[3] = num1.myBytes[0];
+
+  canOut.canBytes[4] = num2.myBytes[3];
+  canOut.canBytes[5] = num2.myBytes[2];
+  canOut.canBytes[6] = num2.myBytes[1];
+  canOut.canBytes[7] = num2.myBytes[0];
 
   return canOut;
 }
