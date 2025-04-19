2ea306b72cb587db56abde3c1d30036e46c6a3dd
bryanmelendez <bryanmelendez2323@gmail.com>
2025-04-19 00:25:18 -0700

    merged mppt

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -7,24 +7,22 @@
 #include "lv_sensors_test.hpp"
 #include "mppt_test.hpp"
 
-// #define TEST_LV
+#define TEST_LV
 #define TEST_MPPT
 
 int main() {
 #ifdef TEST_LV
     LV_TEST lv_test;
     lv_test.currentTest();
+    lv_test.voltageTest();
+    lv_test.tempTest();
+    lv_test.testGPS();
 #endif
     
 #ifdef TEST_MPPT
     MPPT_Test mppt_test;
     mppt_test.powerTest();
     mppt_test.statusTest();
-    LV_TEST lv_test;
-    lv_test.currentTest();
-    lv_test.voltageTest();
-    lv_test.tempTest();
-    lv_test.testGPS();
 #endif
     
     return 0;
